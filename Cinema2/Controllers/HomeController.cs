6439e30f8cc1e4bbf07e12db337cb7db8f52e624
d-jok <31610302+d-jok@users.noreply.github.com>
2019-12-08 23:59:53 +0200

    BackEnd
    Registration and login are created

---
+++ Cinema2/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cinema2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private KinoContext db;
+
+        public HomeController(KinoContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Place()
+        {
+            return View(await db.Place.ToListAsync());
+        }
+
+        public IActionResult CreatePlace()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePlace(BuyTicket obj)
+        {
+            db.Entry(obj).State = EntityState.Added;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cinema2/Models/KinoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema2.Models
+{
+    public class KinoContext : DbContext
+    {
+        public DbSet<BuyTicket> BuyTickets { get; set; }
+        public DbSet<Place> Place { get; set; }
+
+        public KinoContext(DbContextOptions<KinoContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
